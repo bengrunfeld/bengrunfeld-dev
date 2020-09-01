c55a37882792c6152259228b6b67c120e310c687
bengrunfeld <benjamin.grunfeld@gmail.com>
2020-09-01 12:58:45 +0300

    Remove unused config. Test Styled Components

--- components/FormPage/FormPage.styles.ts
+++ components/FormPage/FormPage.styles.ts
@@ -2,4 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div``;
 
-export const Title = styled.p``;
+export const Title = styled.p`
+  font-size: 3rem;
+`;
--- components/FormPage/test/FormPage.test.ts
+++ components/FormPage/test/FormPage.test.ts
@@ -2,7 +2,7 @@ import { add } from "../FormPage";
 
 describe("Add", () => {
   test("should add correctly", () => {
-    const result = add(2, 2);
-    expect(result).toEqual(4);
+    const result = add(3, 2);
+    expect(result).toEqual(5);
   });
 });
