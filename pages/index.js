1689aa54983558d1ca2f1a15e7e60aab12f95d62
bengrunfeld <benjamin.grunfeld@gmail.com>
2020-09-14 14:53:26 +0300

    Add page title

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Head from "next/head";
 import { HomePage } from "../components";
 
-const Home = () => <HomePage />;
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Ben Grunfeld - Full Stack Engineer</title>
+      </Head>
+      <HomePage />
+    </>
+  );
+};
 
 export default Home;
