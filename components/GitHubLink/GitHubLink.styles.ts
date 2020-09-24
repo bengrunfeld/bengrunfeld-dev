ffeef0a3b4aacf8c492c2111ecd2b2da5c356473
bengrunfeld <benjamin.grunfeld@gmail.com>
2020-09-24 12:33:14 +0300

    Update github button styles

--- components/GitHubLink/GitHubLink.styles.ts
+++ components/GitHubLink/GitHubLink.styles.ts
@@ -5,15 +5,15 @@ export const Container = styled.div`
 `;
 
 export const Link = styled.a`
-  width: 20rem;
-  height: 7rem;
+  width: 15rem;
+  height: 6rem;
   display: inline-block;
   padding: 1rem 2rem;
 
   border: 1px solid ${({ theme }) => theme.colors.darkGray};
   background: ${({ theme }) => theme.colors.lightGray};
   background-image: url("/github.png");
-  background-size: 15rem;
+  background-size: 12rem;
   background-position: center center;
   background-repeat: no-repeat;
   border-radius: 0.5rem;
