6004826811d9eec335631703e00509e5450eae0e
bengrunfeld <benjamin.grunfeld@gmail.com>
2020-09-24 13:37:49 +0300

    Hotfix: Text formatting

--- pages/code-examples/index.tsx
+++ pages/code-examples/index.tsx
@@ -15,28 +15,38 @@ const Home = () => {
           they can do something like mobile-first UI, or using libraries like
           Formik and D3, but until you actually see a live version of their
           code, it&apos;s really hard to tell if it&apos;s the truth or not.
+        </PageText>
+
+        <PageText>
           Making things even more complex is that their opinion of what it means
           to &quot;know a library well&quot; might differ vastly from yours.
-          Check out live examples of my code by navigating through the menu.
         </PageText>
 
         <PageText>
           Even having links to personal projects on Github that use said
           libraries or skills isn&apos;t sufficient proof, because unless you
           clone their library and run it locally, you can&apos;t even tell if it
-          really works, or if it looks good. At the end of the day, code is just
-          code. Seeing a live version that is functioning properly is just as
-          important as reading the source code.
+          really works, or if it looks good.{" "}
+        </PageText>
+
+        <PageText>
+          At the end of the day, code is just code. Seeing a live version that
+          is functioning properly is just as important as reading the source
+          code.
         </PageText>
 
         <PageText>
           For these reasons, I set up a Live Code Examples page which achieves a
           few different things. Firstly, it gives me a place to put up live
-          examples of code I think is impressive or useful. It also proves my
-          aptitude in implementing certain libraries and techniques, and it also
-          serves as a central repository for myself. So if I want to remember
-          how to implement a dropdown using Formik and React Select, I can look
-          it up easily and see a working version. Pretty cool, no?
+          examples of code I think is impressive or useful.
+        </PageText>
+
+        <PageText>
+          It also proves my aptitude in implementing certain libraries and
+          techniques, and it also serves as a central repository for myself. So
+          if I want to remember how to implement a dropdown using Formik and
+          React Select, I can look it up easily and see a working version.
+          Pretty cool, no?
         </PageText>
 
         <PageText>Please navigate through the menu to see my work.</PageText>
